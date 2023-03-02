d838fcf8846f9de18aed12c4051f322f4c7a1ac3
Venuslea <104912280+Venuslea@users.noreply.github.com>
2023-03-02 22:06:57 +0800

    Add files via upload

---
+++ exercise5/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    private String name = "Delebat";
+    
+    public String getName() {
+        return this.name;
+}
+
+    public void setName(String personName) {
+        this.name = personName;
+    }
+}
